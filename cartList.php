a0e65b70c27cd5aca75eb217968713d9cf70bd2c
Joanne <prince840414@gmail.com>
2018-03-13 20:51:57 +0800

    list none

--- cartList.php
+++ cartList.php
@@ -202,6 +202,125 @@
         50%  { opacity: .5; }
         100% { opacity: 1; top: -4px ;}
     }
+    /*active-----------------------------------------------------*/
+    .active_f {
+            width:70%;
+            margin: 0 auto;
+            padding: 30px 0;
+        }
+        .active_f ul {
+            display: flex;
+            flex-wrap: wrap; 
+            justify-content: center;   
+        }
+        .active_f ul li {
+            width: 300px;
+            height: 300px;
+            margin-right: 15px;
+            margin-bottom: 15px;
+            position: relative;
+            overflow: hidden;
+            box-sizing: border-box;
+            transition: all 1s ease-in-out; 
+        }
+        .square_f {
+            background: #000000;
+        }
+        .active_f ul li a {
+            width: 300px;
+            height: 300px;
+            display: block;
+            z-index: 2;
+            
+        }
+        .active_f ul .square_f img {
+            max-width: 100%;
+            position: relative;
+            opacity: 0.4;
+            transition: all .5s ease-in-out; 
+        }
+        .square_f p {
+            font-size: 24px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            text-align: center;
+            color: #fff;
+            transition: all .5s ease-in-out;
+            font-weight: 400; 
+
+        }
+        .square_f .p2 {
+            margin-top:-32px;
+            margin-left:-94px;
+        }
+        .square_f .p4 {
+            margin-top:-16px;
+            margin-left:-94px;
+        }
+        .square_f .p6 {
+            margin-top:-16px;
+            margin-left:-81px;
+        }
+        .active_f ul .square_f:before {
+            width: 145%;
+            height: 145%;
+            top: 0;
+            left: 0;
+            content: '';
+            background: #161414;
+            position: absolute;
+            transition: all 0.3s ease-in-out;
+            transform: rotate(110deg) translateY(-31%) translateX(-3.3%);
+        }
+        /* .active_a ul .square_a.red {
+            background: #581a14;
+        } */
+        .active_f ul .square_f:hover:before,
+        .active_f ul .square_f.hover:before {
+            transform: rotate(110deg) translateY(-150%);
+        }
+        .active_f ul .square_f:hover img{
+            opacity: 1;
+            transform: scale(1.1);
+        }
+
+        .active_f ul .square_f:hover p,
+        .active_f ul .square_f.hover p {
+            transform:translate(-160%,0);
+        }
+        .active_f ul .blue {
+            background: #b1aeae;
+        }
+        .active_f ul .orange {
+            background:#e0a340;
+        }
+        h6{
+            text-align: center;
+            font-size: 24px;
+            font-weight: bold;
+            color: #666;
+            margin-top: 50px;
+        }
+        .pulse_f{
+            font-weight: bold;
+            font-size: 24px;
+            text-align: center;
+            width: 50%;
+            margin: 0 auto;
+            color:#C1272D;
+            padding: 100px 0;
+            z-index: 2;
+            animation: pulse .7s infinite alternate;
+        }
+        @keyframes pulse{
+            0%{
+                transform: scale(1);
+            }
+            100%{
+                transform: scale(1.1);
+            }
+        }
     </style>
     <style>
         @import url("css/cartList_phone.css");
@@ -213,10 +332,26 @@
         <p>Be Your Unique Umbrella.</p>
     </div>
     <section class="sec1_a" >
-    <?php if(empty($cartdata)): ?>
-        購物車裡沒有資料
-    <?php else: ?>
         <h1>購物車列表</h1>
+    <?php if(empty($cartdata)): ?>
+        <p class="pulse_f">購物車目前沒有商品</p>
+        <h6>最新活動</h6>
+            <div class="active_f">
+                <ul>
+                    <li class="square_f blue" >
+                            <img class="images" src="images/news_squre2_light.jpg" alt="">
+                            <p class="p2">梅雨季<br>全館雨傘88折</p>
+                            <a href=""></a>   
+                    </li>
+                    <li class="small_pic_f"><img src="images/member_square2.jpg" alt=""></li>
+                    <li class="square_f orange">
+                            <img class="images" src="images/news_squre4_light.jpg" alt="">
+                            <p class="p4">自動傘新品上架</p>
+                            <a href=""></a>
+                    </li>
+                </ul>
+            </div> 
+    <?php else: ?>
         <table class="table_a">
             <thead class="thead-dark_a">
                 <tr>
@@ -276,8 +411,14 @@
                 </tbody>
         </table>
         <div class="total_a" data-val="">共 <span class="tqty">X</span> 件，總金額 NT$. <span class="tprice">2400</span></div>
-    <?php endif; ?>
+        <?php endif; ?>
     </section>
+<?php if(empty($cartdata)): ?>
+    <div class="btn_a">
+        <a class="shop_a" href="home.php">回首頁</a>
+        <a class="buy_a" href="product_list3.php">回商品頁</a>
+    </div>
+<?php else: ?>
     <div class="btn_a">
         <a class="shop_a" href="product_list3.php">繼續購物</a>
         <?php if(isset($_SESSION['user'])): ?>
@@ -286,6 +427,7 @@
         <a class="buy_a" href="member_login.php">登入會員</a>
         <?php endif; ?>
     </div>
+<?php endif; ?>
     <div class="toTop">
         <div class="tr"></div>
         <h5>TOP</h5>
