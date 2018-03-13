e5f461b4278daf1f11d9f8bcd5dce031af2e25ac
YangChihchi <chiyang9925119.gmail.com>
2018-03-14 02:39:26 +0800

    更新

--- fix.php
+++ fix.php
@@ -61,20 +61,17 @@
             margin: 0 auto;
             width: 60%;
             margin-bottom: 45px;  
+            height:300px;
         }
         .umbrellaImg_a {
             position:absolute;
             width: 300px;
             height: 300px;
             background-size: contain;
-            /* transition:all .4s; */
-            opacity:0;
-            transform:translateY(300px);
+            /* transform:translateY(100px); */
+            top:100px;
         }
-        .umbrellaShow_a {
-            opacity:1;
-            transform:translateY(0);
-        }
+
         .umbrellaImg1_a {
             background:url(images/ximage-large-umbrella-01.png,q1510051095.pagespeed.ic.A55E1PNYDs.webp) 50% 50% no-repeat;
         }
@@ -91,10 +88,10 @@
             background:url(images/ximage-large-umbrella-05.png,q1510668587.pagespeed.ic.reu-1cxC6V.webp) 50% 50% no-repeat;
         }
         .umbrellaImgL_a {
-            left: 120px;
+            left: -180px;
         }
         .umbrellaImgR_a {
-            right:120px;
+            right:-180px;
         }
         .umbrellaSvg_a {
             position: absolute;
@@ -147,7 +144,7 @@
             display: block;
             margin:0 auto;
             line-height:1.5;
-            padding-bottom: 10px;
+            /* padding-bottom: 10px; */
         }
 
 
@@ -179,40 +176,44 @@
             </div>
             <h1>注意事項</h1>
             <div class="notice_a">
-                <div class="umbrellaImg_a umbrellaImgL_a umbrellaImg1_a"></div>
-                <div class="umbrellaCard_a">
+                <div class="umbrellaCard_a" data-img="img1">
+                    <div class="umbrellaImg_a umbrellaImgL_a umbrellaImg1_a" id="img1"></div>
                     <div class="innerText_a innerTextL_a">
                         <h2>步驟一</h2>
                         <p>我們的雨傘的設計是為了防風雨。但是，他們不能避免強烈的陣風。避免在大風中使用您的雨傘，特別是海上和河流。</p>
                     </div>
                     <div class="umbrellaSvg_a umbrellaSvgR_a umbrellaSvg1_a"><img src="images/umbrella-care-1.svg" alt=""></div>
                 </div>
-                <div class="umbrellaImg_a umbrellaImgR_a umbrellaImg2_a"></div>
-                <div class="umbrellaCard_a">
+                
+                <div class="umbrellaCard_a" data-img="img2">
+                    <div class="umbrellaImg_a umbrellaImgR_a umbrellaImg2_a" id="img2"></div>
                     <div class="innerText_a innerTextR_a">
                         <h2>步驟二</h2>
                         <p>避免靠在你的雨傘上，把它當作手杖使用。</p>
                     <div class="umbrellaSvg_a umbrellaSvgL_a umbrellaSvg2_a"><img src="images/umbrella-care-2.svg" alt=""></div>
                     </div>
                 </div>
-                <div class="umbrellaImg_a umbrellaImgL_a umbrellaImg3_a"></div>
-                <div class="umbrellaCard_a">
+                
+                <div class="umbrellaCard_a" data-img="img3">
+                    <div class="umbrellaImg_a umbrellaImgL_a umbrellaImg3_a" id="img3"></div>
                     <div class="innerText_a innerTextL_a">
                         <h2>步驟三</h2>
                         <p>請將雨傘打開晾乾傘面能夠透氣，防止任何金屬部件的生鏽。</p>
                     </div>
                     <div class="umbrellaSvg_a umbrellaSvgR_a umbrellaSvg3_a"><img src="images/umbrella-care-3.svg" alt=""></div>
                 </div>
-                <div class="umbrellaImg_a umbrellaImgR_a umbrellaImg4_a"></div>
-                <div class="umbrellaCard_a">
+                
+                <div class="umbrellaCard_a" data-img="img4">
+                    <div class="umbrellaImg_a umbrellaImgR_a umbrellaImg4_a" id="img4"></div>
                     <div class="innerText_a innerTextR_a">
                         <h2>步驟四</h2>
                         <p>讓你的雨傘遠離陽光直射，以防止褪色</p>
                         <div class="umbrellaSvg_a umbrellaSvgL_a umbrellaSvg4_a"><img src="images/umbrella-care-4.svg" alt=""></div>
                     </div>
                 </div>
-                <div class="umbrellaImg_a umbrellaImgL_a umbrellaImg5_a"></div>
-                <div class="umbrellaCard_a">
+                
+                <div class="umbrellaCard_a" data-img="img5">
+                    <div class="umbrellaImg_a umbrellaImgL_a umbrellaImg5_a" id="img5"></div>
                     <div class="innerText_a innerTextL_a">
                         <h2>步驟五</h2>
                         <p>清潔只能用普通水，因為清潔劑會去除防水塗料。</p>
@@ -249,30 +250,24 @@
 
 
        /*notice*/
-        $(document).ready(function() {
-    
-            /* Every time the window is scrolled ... */
-            $(window).scroll( function(){
-            
-                /* Check the location of each desired element */
-                $('.umbrellaImg_a').each( function(i){
-                    
-                    var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-                    var bottom_of_window = $(window).scrollTop() + $(window).height();
-                    
-                    /* If the object is completely visible in the window, fade it it */
-                    if( bottom_of_window > bottom_of_object ){
-                        
-                        $(this).animate({'opacity':'1'},500);
-                        $(this).addClass("umbrellaShow");
-                            
-                    }
-                    
-                }); 
-            
+        var setArea=$(".umbrellaCard_a"),
+            showHeight=50;
+            $(".umbrellaImg_a").css({opacity:"0"});
+           
+            $(window).on('scroll', function(){
+            setArea.each(function(){
+                var setThis=$(this),
+                    areaTop=setThis.offset().top,
+                    umbrellaImg = $(this).attr("data-img");
+                    console.log(areaTop);
+                if($(window).scrollTop()>(areaTop+showHeight)-$(window).height()){
+                    $("#" + umbrellaImg).stop().animate({opacity:"1",top:"0"},400);
+                } else {
+                    $("#" + umbrellaImg).stop().animate({opacity:"0",top:"100px"},400);
+                }
             });
-            
         });
+
 
     </script>
 <?php include __DIR__.'/module_foot.php' ?>
