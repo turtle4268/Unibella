50ae242279521288e539c6b4c2f67b5a07701971
Joanne <prince840414@gmail.com>
2018-03-15 17:49:26 +0800

    none list

--- member_center_savelist.php
+++ member_center_savelist.php
@@ -157,9 +157,19 @@
             position: relative;
             margin : 5% 0;
         }
-        .shop_a{
+        .btn_a a {
             margin:15px 45px;
             border-radius: 28px;
+            padding:10px 20px;
+            border: 2px solid #302f3a;
+            background: transparent;
+            color:  #302f3a;
+            font-size: 1rem;
+            position: relative;
+            cursor:pointer;
+            display: block ;
+        }
+        .shop_a{
             padding:10px 40px;
             border: 2px solid #302f3a;
             background: transparent;
@@ -168,8 +178,6 @@
             position: relative;
         }
         .buy_a{
-            margin:15px 45px;
-            border-radius: 28px;
             padding:10px 40px;
             border: 2px solid #302f3a;
             background: transparent;
@@ -177,13 +185,13 @@
             font-size: 1rem;
             position: relative;
         }
-        .btn_a button:hover {
+        .btn_a a:hover {
             color: #fff;
             transition: color 200ms linear 200ms;
             -webkit-transition: color 200ms linear 200ms;
             z-index: 3;
         }
-        .btn_a .shop_a::before {
+        .btn_a .shop_a:before {
             content: '';
             position: absolute;
             width: 100%;
@@ -195,7 +203,7 @@
             border-radius: 28px;
             transition: all 200ms ease-out;
         }
-        .btn_a .buy_a::before {
+        .btn_a .buy_a:before {
             content: '';
             position: absolute;
             width: 100%;
@@ -207,14 +215,14 @@
             border-radius: 28px;
             transition: all 200ms ease-out;
         }
-        .btn_a button:hover::before {
+        .btn_a a:hover::before {
             top: 0;
             left: 0;
         }
-        .btn_a button:active {
+        .btn_a a:active {
             transform: translateY(3px);
         }
-        .btn_a button:focus {
+        .btn_a a:focus {
             outline: none;
         }
        /*-------toTop--------*/
@@ -244,11 +252,130 @@
             color: #1F5572 ;
             text-align: center;
         }
-    @keyframes scroll_top {
-        0%   { opacity: .2; top: 2px; }
-        50%  { opacity: .5; }
-        100% { opacity: 1; top: -4px ;}
-    }
+        @keyframes scroll_top {
+            0%   { opacity: .2; top: 2px; }
+            50%  { opacity: .5; }
+            100% { opacity: 1; top: -4px ;}
+        }
+        /*active-----------------------------------------------------*/
+        .active_f {
+            width:100%;
+            margin: 0 auto;
+            padding: 30px 0;
+        }
+        .active_f ul {
+            display: flex;
+            flex-wrap: wrap; 
+            justify-content: center;   
+        }
+        .active_f ul li {
+            width: 300px;
+            height: 300px;
+            margin-right: 15px;
+            margin-bottom: 15px;
+            position: relative;
+            overflow: hidden;
+            box-sizing: border-box;
+            transition: all 1s ease-in-out; 
+        }
+        .square_f {
+            background: #000000;
+        }
+        .active_f ul li a {
+            width: 300px;
+            height: 300px;
+            display: block;
+            z-index: 2;
+            
+        }
+        .active_f ul .square_f img {
+            max-width: 100%;
+            position: relative;
+            opacity: 0.4;
+            transition: all .5s ease-in-out; 
+        }
+        .square_f p {
+            font-size: 24px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            text-align: center;
+            color: #fff;
+            transition: all .5s ease-in-out;
+            font-weight: 400; 
+
+        }
+        .square_f .p2 {
+            margin-top:-32px;
+            margin-left:-94px;
+        }
+        .square_f .p4 {
+            margin-top:-16px;
+            margin-left:-94px;
+        }
+        .square_f .p6 {
+            margin-top:-16px;
+            margin-left:-81px;
+        }
+        .active_f ul .square_f:before {
+            width: 145%;
+            height: 145%;
+            top: 0;
+            left: 0;
+            content: '';
+            background: #161414;
+            position: absolute;
+            transition: all 0.3s ease-in-out;
+            transform: rotate(110deg) translateY(-31%) translateX(-3.3%);
+        }
+        /* .active_a ul .square_a.red {
+            background: #581a14;
+        } */
+        .active_f ul .square_f:hover:before,
+        .active_f ul .square_f.hover:before {
+            transform: rotate(110deg) translateY(-150%);
+        }
+        .active_f ul .square_f:hover img{
+            opacity: 1;
+            transform: scale(1.1);
+        }
+
+        .active_f ul .square_f:hover p,
+        .active_f ul .square_f.hover p {
+            transform:translate(-160%,0);
+        }
+        .active_f ul .blue {
+            background: #b1aeae;
+        }
+        .active_f ul .orange {
+            background:#e0a340;
+        }
+        h6{
+            text-align: center;
+            font-size: 24px;
+            font-weight: bold;
+            color: #666;
+            margin-top: 50px;
+        }
+        .pulse_f{
+            font-weight: bold;
+            font-size: 24px;
+            text-align: center;
+            width: 50%;
+            margin: 0 auto;
+            color:#C1272D;
+            padding: 100px 0;
+            z-index: 2;
+            animation: pulse .7s infinite alternate;
+        }
+        @keyframes pulse{
+            0%{
+                transform: scale(1);
+            }
+            100%{
+                transform: scale(1.1);
+            }
+        }
     </style>
     <style>
         @import url("css/member_center_savelist_phone.css");
@@ -264,6 +391,25 @@
         
             <div class="main_y">
                 <h2>收藏列表</h2>
+        <?php if(empty($loves)): ?>
+                <p class="pulse_f">收藏庫目前沒有商品</p>
+                <h6>最新活動</h6>
+                <div class="active_f">
+                    <ul>
+                        <li class="square_f blue" >
+                            <img class="images" src="images/news_squre2_light.jpg" alt="">
+                            <p class="p2">梅雨季<br>全館雨傘85折</p>
+                            <a href=""></a>   
+                        </li>
+                        <li class="small_pic_f"><img src="images/member_square2.jpg" alt=""></li>
+                        <li class="square_f orange">
+                            <img class="images" src="images/news_squre4_light.jpg" alt="">
+                            <p class="p4">自動傘新品上架</p>
+                            <a href=""></a>
+                        </li>
+                    </ul>
+                </div> 
+        <?php else: ?>
                 <table class="tableSave_y">
                     <thead>
                         <tr>
@@ -280,12 +426,12 @@
                             <td class="save_pic_f"><img src="images/detail/<?= $value['umbrella_id'] ?>_1.png" alt=""></td>
                             <td><?= $value['umbrellaname'] ?></td>
                             <td>NT$.<?= $value['price'] ?></td>
-                            <td><button class="addcart_f">加入購物車</button></td>
-                            <td><button class="delete_f"><i class="far fa-trash-alt"></i></button></td>
+                            <td><a class="addcart_f cartbtn">加入購物車</a></td>
+                            <td><a class="delete_f"><i class="far fa-trash-alt delet"></i></a></td>
                         </tr>
                 <?php endforeach; ?>
                     </tbody>
-                    <table class="tablePhone_a">
+                </table>
                 <?php foreach($loves as $value): ?>
                     <div class="tablePhone_a card" data-sid="<?= $value['product_sid'] ?>">
                         <div class="listTitle_a">商品圖片</div>
@@ -293,17 +439,16 @@
                         <div class="listContent_a">
                             <p>商品名稱<span class="listProduct_a"><?= $value['umbrellaname'] ?></span></p>
                             <p>單價<span class="listCS_a price">NT$.<?= $value['price'] ?></span></p>
-                            <p class="listContentAD_a"><a>加入購物車</a></p>
-                            <p class="listContentAD_a"><a>刪除</a></p>
+                            <p class="listContentAD_a cartbtn"><a>加入購物車</a></p>
+                            <p class="listContentAD_a delet"><a>刪除</a></p>
                         </div>
                     </div>
                 <?php endforeach; ?>
-                     
-                 </table>
-                </table> 
+                 
+        <?php endif; ?>
                 <div class="btn_a">
-                    <button class="shop_a">繼續購物</button>
-                    <button class="buy_a">前往購物車</button>
+                    <a class="shop_a" href="product_list3.php">繼續購物</a>
+                    <a class="buy_a" href="cartList.php">前往購物車</a>
                 </div> 
             </div> <!--main_y-->
         </div> <!--mainBox_y-->
@@ -341,6 +486,32 @@
         $(".fa-angle-down").click(function () {
             $(".fa-angle-down").toggleClass("rotate");
             $(".l_p_ul_y").slideToggle();
+        });
+         /*add to cart*/
+         $(".cartbtn").click(function(){
+            var card=$(this).closest(".card");
+            var sid=card.data('sid');  
+            var qty=1;
+
+            $.get('add_to_cart.php',{sid:sid,qty:qty},function(data){
+                console.log(data);
+                alert("商品已加入購物車");
+                // countItems(data);
+            },"json");
+        });
+        /*delet*/
+        $(".delet").click(function(){
+            var card=$(this).closest(".card");
+            var sid=card.data("sid");
+            
+            $.get('add_to_love.php',{sid:sid},function(data){
+                $(".card").each(function(){
+                    if($(this).data("sid")==sid) $(this).remove();
+                });
+            },'json');
+            <?php if(empty($loves)): ?>
+                location.href=location.href;
+            <?php endif; ?>
         });
     </script>
 <?php include __DIR__.'/module_foot.php' ?>
