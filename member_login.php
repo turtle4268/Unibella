1eb47732e9b02a403b3c9333ba8e0d91410cdb9c
Anna <chiyang9925119@gmail.com>
2018-03-16 15:24:04 +0800

    focus

--- member_login.php
+++ member_login.php
@@ -1,6 +1,6 @@
 <?php require __DIR__. '/_db_connect.php'; ?>
 <?php 
-    $_SESSION['come_from'] = $_SERVER['HTTP_REFERER'];
+    // $_SESSION['come_from'] = $_SERVER['HTTP_REFERER'];
 ?>
 <?php include __DIR__.'/module_head.php' ?>
     <style>
@@ -93,13 +93,14 @@
         }
         .loginTitle_y{
             color: #1f5572;
-            margin-bottom: 3em;
+            margin-bottom: 2em;
         }
         .loginLabel_y{
             color: #1f5572;
             font-size: 16px;
             /* flex: 2; */
-            margin-right:5px;
+            /* margin-right:5px; */
+            margin-left:10px;
         }
         input{
             font-size:16px;
@@ -114,7 +115,7 @@
              width: 200px;
         }
         .loginPass_y{
-            width: 220px;
+            width: 100%;
         }
         input:focus{
             outline:none!important;
@@ -126,7 +127,7 @@
             border: 2px solid #1f5572;
             border-radius: 50px;
             padding: 3px 90px;
-            margin-top: 1.5em;
+            margin-top: 2em;
         }
         .loginBtn_y:hover, .registerPBtn_y:hover{
             background-color: #1f5572;
@@ -137,7 +138,7 @@
              /* display: flex;
              justify-content: space-between;
              align-items: center; */
-             margin: 40px 20px; 
+             margin:0 10px; 
              text-align: left;
         }
         .registerP_Title_y{
@@ -423,6 +424,47 @@
         .check .box.checked{
             -webkit-mask-image: url(images/member_checked_yellow.svg) ;
         }
+        /*--border--*/
+        :focus{
+        outline: none;
+        }
+        .col-3{
+            width: 65%; 
+            position: relative;
+        } 
+        input[type="text"]{
+            font: 15px/24px "Lato", Arial, sans-serif; 
+            color: #333; 
+            box-sizing: border-box; 
+            letter-spacing: 1px;
+        }
+        .effect-1 {
+            border: 0; 
+            border-bottom: 1px solid #aaa;
+        }
+        .effect-1 ~ .focus-border{
+            position: absolute; 
+            bottom: 0; 
+            left: 0; 
+            width: 0; 
+            height: 1px; 
+            background-color: #1f5572; 
+            transition: 0.4s;
+        }
+        .effect-1:focus ~ .focus-border{
+            width: 100%; 
+            transition: 0.4s;
+        }
+        .labelOut_a {
+            display:flex;
+            margin: 27px 0 43px 0;
+        }
+        .col2-3 {
+            width:71%;
+        }
+        .form_login {
+            margin-top:20px;
+        }
     </style>
     <style>
         @import url("css/member_login_phone.css");
@@ -534,17 +576,21 @@
                         <div class="login2_y">
                             <h2 class="loginTitle_y">會員登入</h2>
                             <form name="form_login" method="post" action="" onsubmit="return loginCheck()">
-                                <div class="loginInfor_y">
+                                <div class="labelOut_a">
                                     <label for="email_login" class="loginLabel_y">Email:</label>
-                                    <input type="text" class="loginInput_y" name="email_login" id="email_login" value="<?= isset($_SESSION['remember'])?$_SESSION['remember']:'' ?>" placeholder="" width:100px;>
-                                    <!-- <small id="emailWarning" class="form-text text-muted warning">請填寫正確的電郵</small>
-                                                                                 -->
+                                    <div class="loginInfor_y col-3">
+                                        <input type="text" class="loginInput_y effect-1" name="email_login" id="email_login" value="<?= isset($_SESSION['remember'])?$_SESSION['remember']:'' ?>" placeholder="" width:100px;>
+                                        <span class="focus-border"></span>
+                                            <!-- <small id="emailWarning" class="form-text text-muted warning">請填寫正確的電郵</small>-->
+                                    </div>
                                 </div>
-                                <div class="loginInfor_y">
+                                <div class="labelOut_a">
                                     <label for="password_login" class="loginLabel_y">密碼:</label>
-                                    <input type="password" class="loginInput_y loginPass_y" name="password_login" id="password_login" value="" placeholder="">
-                                    <!-- <small id="passwordWarning" class="form-text text-muted warning">請輸入六個字元以上的密碼</small> -->
-                    
+                                    <div class="loginInfor_y col-3 col2-3">
+                                        <input type="password" class="loginInput_y loginPass_y  effect-1" name="password_login" id="password_login" value="" placeholder="">
+                                        <span class="focus-border"></span>
+                                        <!-- <small id="passwordWarning" class="form-text text-muted warning">請輸入六個字元以上的密碼</small> -->
+                                    </div>
                                 </div>
                                 <div class="rememberBox_y">
                                     <div class="check">
