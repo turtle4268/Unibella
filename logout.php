e7d22d00ed159c7faf45c76c0905df54fb42fa2a
turtle4268 <turtle4268@yahoo.com.tw>
2018-03-23 13:59:51 +0800

    登出清cart

--- logout.php
+++ logout.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
-unset($_SESSION['user']);unset($_SESSION['love']);
+unset($_SESSION['user']);unset($_SESSION['love']);unset($_SESSION['cart']);
+// session_destroy();
 
 header("Location: ./home.php");
